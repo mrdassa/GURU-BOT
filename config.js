fe536655248a29463cb79c84a591cb89f9bb6a98
mrdassa <165574467+mrdassa@users.noreply.github.com>
2024-04-11 09:20:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94719750429', 'mr.', true],
+  ['94719750429', 'sawwa', true], 
   [''] 
 ] //Number of owners
 
